d9b06283e8edef88ffedca0ad315842e573dcd69
Roger Enand <rogerenand@gmail.com>
2019-09-19 15:18:11 -0700

    added link, styled active page links, made .red bigger

--- script.js
+++ script.js
@@ -10,18 +10,26 @@ function clickEvent(){
         case "See My Work":
             $('.work_content').css('display','flex');
             $('.intro_content, .about_me_content, .contact_content').css('display','none');
+            $('.work_link').css('color','red');
+            $('.home_link, .about_link, .contact_link').css('color','white');
             break;
         case "About Me":
             $('.about_me_content').css('display','flex');
             $('.intro_content, .work_content, .contact_content').css('display','none');
+            $('.about_link').css('color','red');
+            $('.home_link, .contact_link, .work_link').css('color','white');
             break;
         case "Contact":
             $('.contact_content').css('display','flex');
             $('.intro_content, .work_content, .about_me_content').css('display','none');
+            $('.contact_link').css('color','red');
+            $('.home_link, .about_link, .work_link').css('color','white');
             break;
         default:
             $('.intro_content').css('display','flex');
-            $('.work_content, .about_me_content, .contact_content').css('display','none');  
+            $('.work_content, .about_me_content, .contact_content').css('display','none');
+            $('.home_link').css('color','red');
+            $('.about_link, .contact_link, .work_link').css('color','white'); 
             break;    
     }
 
