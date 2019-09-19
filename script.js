fe2b1eb8d6e26f0def679a9ca9f9422cf21ba940
Roger Enand <rogerenand@gmail.com>
2019-09-19 05:47:59 -0700

    added pages

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 $('document').ready(initApp);
 function initApp(){
-    $('button').click(clickEvent);
+    // $('.intro_content').css('display','flex');
+    $('.button, .nav_link_item').click(clickEvent);
 }
 function clickEvent(){
-    $('.main_content').css('display','flex');
-    $('.intro_content').css('display','none');
+    var theSelected = this.innerText;
+    console.log(theSelected);
+    switch(theSelected){
+        case "See My Work":
+            $('.work_content').css('display','flex');
+            $('.intro_content, .about_me_content, .contact_content').css('display','none');
+            break;
+        case "About Me":
+            $('.about_me_content').css('display','flex');
+            $('.intro_content, .work_content, .contact_content').css('display','none');
+            break;
+        case "Contact":
+            $('.contact_content').css('display','flex');
+            $('.intro_content, .work_content, .about_me_content').css('display','none');
+            break;
+        default:
+            $('.intro_content').css('display','flex');
+            $('.work_content, .about_me_content, .contact_content').css('display','none');  
+            break;    
+    }
+
 }
