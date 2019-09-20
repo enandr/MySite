7e7eb04187b1652edaada9c59074a46e439a834d
Roger Enand <rogerenand@gmail.com>
2019-09-20 00:09:49 -0700

    started responsoveness

--- script.js
+++ script.js
@@ -4,32 +4,34 @@ function initApp(){
     $('.submit_btn').click(formSubmit);
 }
 function clickEvent(){
+    var textMainColor = getComputedStyle(document.documentElement).getPropertyValue('--main_text_color');
+    var textAltColor = getComputedStyle(document.documentElement).getPropertyValue('--main_text_color_alt');
     var theSelected = this.innerText;
     console.log(theSelected);
     switch(theSelected){
         case "See My Work":
             $('.work_content').css('display','flex');
             $('.intro_content, .about_me_content, .contact_content').css('display','none');
-            $('.work_link').css('color','red');
-            $('.home_link, .about_link, .contact_link').css('color','white');
+            $('.work_link').css('color',textAltColor);
+            $('.home_link, .about_link, .contact_link').css('color',textMainColor);
             break;
         case "About Me":
             $('.about_me_content').css('display','flex');
             $('.intro_content, .work_content, .contact_content').css('display','none');
-            $('.about_link').css('color','red');
-            $('.home_link, .contact_link, .work_link').css('color','white');
+            $('.about_link').css('color',textAltColor);
+            $('.home_link, .contact_link, .work_link').css('color',textMainColor);
             break;
         case "Contact":
             $('.contact_content').css('display','flex');
             $('.intro_content, .work_content, .about_me_content').css('display','none');
-            $('.contact_link').css('color','red');
-            $('.home_link, .about_link, .work_link').css('color','white');
+            $('.contact_link').css('color',textAltColor);
+            $('.home_link, .about_link, .work_link').css('color',textMainColor);
             break;
         default:
             $('.intro_content').css('display','flex');
             $('.work_content, .about_me_content, .contact_content').css('display','none');
-            $('.home_link').css('color','red');
-            $('.about_link, .contact_link, .work_link').css('color','white'); 
+            $('.home_link').css('color',textAltColor);
+            $('.about_link, .contact_link, .work_link').css('color',textMainColor); 
             break;    
     }
 
